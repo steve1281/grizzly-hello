be3485d7b13dcab228cc3c8da9c70e973fb47495
Steve Falcigno <steve1281@hotmail.com>
2018-03-30 16:56:36 -0400

    debug stuff

--- src/main/java/com/sfalcigno/grizzly/endpoints/HelloWorld.java
+++ src/main/java/com/sfalcigno/grizzly/endpoints/HelloWorld.java
@@ -4,12 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
- 
+
 @Path("helloworld")
 public class HelloWorld {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
+        System.out.println("helloworld - getIt called");
         return "Hello World.";
     }
 }
